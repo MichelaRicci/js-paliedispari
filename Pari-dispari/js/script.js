cd4e559af850b88fffeb12bacd8e426f3834950e
MichelaRicci <michela.ricci.1991@gmail.com>
2022-12-15 14:55:56 +0100

    form html

--- Pari-dispari/js/script.js
+++ Pari-dispari/js/script.js
@@ -1,4 +1,16 @@
 console.log('JS OK');
 
 
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5 (FORM);
+// Generiamo un numero random (da 1 a 5) per il computer (FUNZIONE);
+// Sommiamo i due numeri;
+// Stabiliamo se la somma dei due numeri è pari o dispari (FUNZIONE);
+// Dichiariamo chi ha vinto.
+
+
+// Recupero elementi dal form 
+
+const evenOdd = document.getElementById('evenodd');
+
+const numberFromUser = document.getElementById('numberFromUser');
 
